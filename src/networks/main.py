36a0bfffd0cdd902154b864c2e1dd864b1803b99
Jack <jackgisby@gmail.com>
2021-06-21 16:54:37 +0100

    Update network selection to allow transformer choice and specification of network features

--- src/networks/main.py
+++ src/networks/main.py
@@ -1,29 +1,36 @@
 from .basic_multilayer import BasicMultilayer, BasicMultilayerAutoencoder
+from .cocrystal_transformer import CocrystalTransformer, CocrystalTransformerAutoencoder
 
 
-def build_network(net_name):
+def build_network(net_name, rep_dim, in_features):
     """Builds the neural network."""
 
-    implemented_networks = ('basic_multilayer',)
+    implemented_networks = ("basic_multilayer", "cocrystal_transformer")
     assert net_name in implemented_networks
 
     net = None
 
-    if net_name == 'basic_multilayer':
-        net = BasicMultilayer()
+    if net_name == "basic_multilayer":
+        net = BasicMultilayer(rep_dim=rep_dim, in_features=in_features)
+
+    elif net_name == "cocrystal_transformer":
+        net = CocrystalTransformer(rep_dim=rep_dim, in_features=in_features)
 
     return net
 
 
-def build_autoencoder(net_name):
+def build_autoencoder(net_name, rep_dim, in_features):
     """Builds the corresponding autoencoder network."""
 
-    implemented_networks = ('basic_multilayer',)
+    implemented_networks = ("basic_multilayer", "cocrystal_transformer")
     assert net_name in implemented_networks
 
     ae_net = None
 
-    if net_name == 'basic_multilayer':
-        ae_net = BasicMultilayerAutoencoder()
+    if net_name == "basic_multilayer":
+        ae_net = BasicMultilayerAutoencoder(rep_dim=rep_dim, in_features=in_features)
+
+    elif net_name == "cocrystal_transformer":
+        ae_net = CocrystalTransformerAutoencoder(rep_dim=rep_dim, in_features=in_features)
 
     return ae_net
