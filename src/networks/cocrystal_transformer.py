7597afca0253d05bceea55931e399277f9cef3d6
Jack <jackgisby@gmail.com>
2021-06-21 16:49:52 +0100

    Add a transformer from the cocrystal paper as an alternative architecture

---
+++ src/networks/cocrystal_transformer.py
@@ -0,0 +1,109 @@
+from math import sqrt
+import torch
+import torch.nn as nn
+
+from base.base_net import BaseNet
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
+
+
+class CocrystalTransformer(BaseNet):
+
+    def __init__(self, rep_dim, in_features):
+        super().__init__()
+
+        self.rep_dim = rep_dim
+        self.in_features = in_features
+
+        self.seq = nn.Sequential(SAB(dim_in=self.in_features, dim_out=1000, num_heads=10),
+                                 SAB(dim_in=1000, dim_out=500, num_heads=5),
+                                 SAB(dim_in=500, dim_out=self.rep_dim, num_heads=10),
+                                 PMA(dim=self.rep_dim, num_heads=5, num_seeds=1))
+
+    def forward(self, x):
+        x = torch.split(x, self.in_features, dim=1)
+        x = torch.stack(x).transpose(0, 1)
+        return self.seq(x).squeeze()
+
+
+class CocrystalTransformerAutoencoder(CocrystalTransformer):
+
+    def __init__(self, rep_dim, in_features):
+        super().__init__(rep_dim=rep_dim, in_features=in_features)
+
+        self.encoder = CocrystalTransformer(rep_dim=rep_dim, in_features=in_features)
+        self.encoder.apply(init_weights)
+
+        self.decoder = nn.Sequential(nn.Linear(in_features=self.rep_dim, out_features=1000), nn.LeakyReLU(),
+                                     nn.Linear(in_features=1000, out_features=self.in_features), nn.Sigmoid())
+        self.decoder.apply(init_weights)
+
+    def forward(self, x):
+        return self.decoder(self.encoder(x))
+
+
+class MAB(nn.Module):
+    def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
+        super(MAB, self).__init__()
+        self.dim_V = dim_V
+        self.num_heads = num_heads
+        self.fc_q = nn.Linear(dim_Q, dim_V)
+        self.fc_k = nn.Linear(dim_K, dim_V)
+        self.fc_v = nn.Linear(dim_K, dim_V)
+        if ln:
+            self.ln0 = nn.LayerNorm(dim_V)
+            self.ln1 = nn.LayerNorm(dim_V)
+        self.fc_o = nn.Linear(dim_V, dim_V)
+
+    def forward(self, Q, K):
+        Q = self.fc_q(Q)
+        K, V = self.fc_k(K), self.fc_v(K)
+
+        dim_split = self.dim_V // self.num_heads
+        Q_ = torch.cat(Q.split(dim_split, 2), 0)
+        K_ = torch.cat(K.split(dim_split, 2), 0)
+        V_ = torch.cat(V.split(dim_split, 2), 0)
+
+        A = torch.softmax(Q_.bmm(K_.transpose(1,2))/sqrt(self.dim_V), 2)
+        O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
+        O = O if getattr(self, 'ln0', None) is None else self.ln0(O)
+        O = O + torch.nn.functional.relu(self.fc_o(O))
+        O = O if getattr(self, 'ln1', None) is None else self.ln1(O)
+        return O
+
+
+class SAB(nn.Module):
+    def __init__(self, dim_in, dim_out, num_heads, ln=False):
+        super(SAB, self).__init__()
+        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln)
+
+    def forward(self, X):
+        return self.mab(X, X)
+
+
+class ISAB(nn.Module):
+    def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False):
+        super(ISAB, self).__init__()
+        self.I = nn.Parameter(torch.Tensor(1, num_inds, dim_out))
+        nn.init.xavier_uniform_(self.I)
+        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln)
+        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln)
+
+    def forward(self, X):
+        H = self.mab0(self.I.repeat(X.size(0), 1, 1), X)
+        return self.mab1(X, H)
+
+
+class PMA(nn.Module):
+    def __init__(self, dim, num_heads, num_seeds, ln=False):
+        super(PMA, self).__init__()
+        self.S = nn.Parameter(torch.Tensor(1, num_seeds, dim))
+        nn.init.xavier_uniform_(self.S)
+        self.mab = MAB(dim, dim, dim, num_heads, ln=ln)
+
+    def forward(self, X):
+        return self.mab(self.S.repeat(X.size(0), 1, 1), X)
