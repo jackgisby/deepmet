241d93453ee49911f641c72dce8f7c05078c914e
Jack <jackgisby@gmail.com>
2021-06-21 16:52:09 +0100

    Allow specification of the model's input features and representation dimension

--- src/networks/basic_multilayer.py
+++ src/networks/basic_multilayer.py
@@ -6,21 +6,21 @@ from base.base_net import BaseNet
 
 class BasicMultilayer(BaseNet):
 
-    def __init__(self):
+    def __init__(self, rep_dim, in_features):
         super().__init__()
 
-        self.in_features = 2048  # in features
-        self.rep_dim = 50  # autoencoder representation size
+        self.rep_dim = rep_dim  # autoencoder representation size
+        self.in_features = in_features  # in features
 
         # encoder
-        self.deep1 = nn.Linear(self.in_features, 250)
-        self.deep2 = nn.Linear(250, 100)
-        self.fc_output = nn.Linear(100, self.rep_dim)
+        self.deep1 = nn.Linear(self.in_features, 500)
+        self.deep2 = nn.Linear(500, 250)
+        self.fc_output = nn.Linear(250, self.rep_dim)
 
         # decoder
-        self.deep4 = nn.Linear(self.rep_dim, 100)
-        self.deep5 = nn.Linear(100, 250)
-        self.deep6 = nn.Linear(250, self.in_features)
+        self.deep4 = nn.Linear(self.rep_dim, 250)
+        self.deep5 = nn.Linear(250, 500)
+        self.deep6 = nn.Linear(500, self.in_features)
 
     def forward(self, x):
 
@@ -33,8 +33,8 @@ class BasicMultilayer(BaseNet):
 
 class BasicMultilayerAutoencoder(BasicMultilayer):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, rep_dim, in_features):
+        super().__init__(rep_dim=rep_dim, in_features=in_features)
 
     def forward(self, x):
 
