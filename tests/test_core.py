4f886243c5191ff64b13b762fe413a133a18e030
Jack Gisby <jackgisby@gmail.com>
2022-07-13 13:27:22 +0100

    Add core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -22,6 +22,7 @@ import unittest
 
 from tests.utils import *
 from deepmet.core import *
+from deepmet.networks import CocrystalTransformer
 
 
 class CoreTestCase(unittest.TestCase):
@@ -37,8 +38,42 @@ class CoreTestCase(unittest.TestCase):
         # create temporary directory for testing
         cls.temp_results_dir = make_temp_results_dir()
 
-    def test_(self):
-        pass
+    def test_deepmet(self):
+
+        # some parameters aren't allowed
+        with self.assertRaises(AssertionError):
+            DeepMet(nu=-1)
+
+        with self.assertRaises(AssertionError):
+            DeepMet(objective="two-class")
+
+        # create DeepMet class
+        deepmet = DeepMet(rep_dim=200, in_features=2800)
+
+        # set network
+        deepmet.set_network("cocrystal_transformer")
+        self.assertIsInstance(deepmet.net, CocrystalTransformer)
+
+        # load a model
+        deepmet.load_model(self.to_test_results("data", "models", "deep_met_model.tar"))
+        self.assertEqual(deepmet.R, 0)
+        self.assertAlmostEqual(sum(deepmet.c), 15.8816223219, places=5)
+
+        deepmet.save_model(self.to_test_results("reloaded_deep_met_model.tar"))
+
+        # reload model, check again
+        deepmet_reloaded = DeepMet(rep_dim=200, in_features=2800)
+        deepmet_reloaded.set_network("cocrystal_transformer")
+        deepmet_reloaded.load_model(self.to_test_results("reloaded_deep_met_model.tar"))
+
+        self.assertEqual(deepmet_reloaded.R, 0)
+        self.assertAlmostEqual(sum(deepmet_reloaded.c), 15.8816223219, places=5)
+        self.assertEqual(str(deepmet.net), str(deepmet_reloaded.net))
+
+    def test_get_radius(self):
+
+        c = torch.ones(200, device="cpu") * 10
+        self.assertAlmostEqual(get_radius(c, 0.5), 3.1622776985168457, places=5)
 
 
 if __name__ == '__main__':
