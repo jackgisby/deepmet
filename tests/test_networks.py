0727e3f13c53c5b09b5229e16f766a5215fcfebf
Jack Gisby <jackgisby@gmail.com>
2022-07-13 13:27:48 +0100

    Add networks tests

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -37,8 +37,25 @@ class NetworksTestCase(unittest.TestCase):
         # create temporary directory for testing
         cls.temp_results_dir = make_temp_results_dir()
 
-    def test_(self):
-        pass
+    def test_build_network(self):
+
+        # try building basic network
+        net = build_network("basic_multilayer", 200, 2800)
+        self.assertIsInstance(net.forward(torch.Tensor(50, 2800)), torch.Tensor)
+
+        # try building transformer network
+        net = build_network("cocrystal_transformer", 200, 2800)
+        self.assertIsInstance(net.forward(torch.Tensor(50, 2800)), torch.Tensor)
+
+    def test_networks(self):
+
+        # try building basic network
+        net = BasicMultilayer(200, 2800)
+        self.assertIsInstance(net.forward(torch.Tensor(50, 2800)), torch.Tensor)
+
+        # try building transformer network
+        net = CocrystalTransformer(200, 2800)
+        self.assertIsInstance(net.forward(torch.Tensor(50, 2800)), torch.Tensor)
 
 
 if __name__ == '__main__':
